9017d442dd3fb3c5f751897d940b49c59ae036bb
abdulwahi <31159281+abdulwahi@users.noreply.github.com>
2021-09-11 15:40:04 +0530

    Poll check

--- master.groovy
+++ master.groovy
@@ -1,11 +1,17 @@
 job('demo') {
     steps {
-        shell('echo Hello World! && echo $USER && pwd && ls')
+        shell('echo Hello Jeyender! && echo $USER && pwd && ls')
     }
 }
 
 job('demo2') {
     steps {
-        shell('echo Hello World! && echo $USER && pwd && ls')
+        shell('echo Hello Abdul! && echo $USER && pwd && ls')
+    }
+}
+
+job('demo3') {
+    steps {
+        shell('echo Hello Abdul! && echo $USER && pwd && ls')
     }
 }
